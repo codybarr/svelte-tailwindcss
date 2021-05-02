b9d9f66b27991084f1954eb71575c1815248cf08
Cody Barr <cody.barr@gmail.com>
2021-05-02 00:55:55 -0600

    initial commit 🥃

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { onMount } from 'svelte'
+  import Tailwind from './Tailwind.svelte'
+
+  let time = new Date()
+
+  // these automatically update when `time`
+  // changes, because of the `$:` prefix
+  $: hours = time.getHours()
+  $: minutes = time.getMinutes()
+  $: seconds = time.getSeconds()
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      time = new Date()
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  })
+</script>
+
+<div class="flex align-center justify-center h-screen">
+  <div class="shadow-lg bg-gray-500 text-white p-5">
+    <p>{time}</p>
+  </div>
+</div>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import App from './App.svelte'
+
+import 'tailwindcss/tailwind.css'
+
+var app = new App({
+	target: document.body,
+})
+
+export default app
